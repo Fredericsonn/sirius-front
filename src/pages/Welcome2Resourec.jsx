28c2322d79128bef93d5516656faee6b28eb4f6f
khalil <khalilaraoui1@gmail.com>
2025-02-07 14:06:02 +0100

    update

--- src/pages/Welcome2Resourec.jsx
+++ src/pages/Welcome2Resourec.jsx
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { redirect, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const ResourceWelcome = () => {
     const [currentImage, setCurrentImage] = useState(0);
+    const user = useSelector((state) => state.userState.user);
+    const navigate = useNavigate();
 
     const images = [
         "https://cnm.fr/wp-content/uploads/2023/01/transition-ecologique-cnm-1.jpg",
@@ -14,6 +19,16 @@ const ResourceWelcome = () => {
         }, 5000); 
         return () => clearInterval(interval);
     }, []);
+
+    const goToResources = () => {
+        
+        if (user) navigate('/resource/dashboard');
+        
+        else {
+            toast.error("You must be logged to view your resources", {autoClose: 2000});
+            return navigate('/login');
+        }
+    }
 
     return (
         <div className="relative w-full h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-800">
@@ -28,12 +43,11 @@ const ResourceWelcome = () => {
                <h1 className="text-3xl font-bold mb-4">Welcome to Your Hardware Ecosystem</h1>
                 <p className="text-lg mb-6">
                    Explore your hardware ecosystem like never before! This revolutionary application allows you to dive into the heart of machines, discovering the hardware resources of their components. Visualize, analyze, and track their consumption or collection at a glance for simplified and optimized management.
-               </p>
-                     <Link to="/resource/dashboard">
-                    <button className="px-6 py-3 bg-green-600 text-white rounded-lg hover:bg-green-700 transition">
+               </p>                   
+                    <button className="px-6 py-3 bg-green-600 text-white rounded-lg hover:bg-green-700 transition"
+                            onClick={goToResources}>
                         Discover
                     </button>
-                </Link>
             </div>
         </div>
     );
